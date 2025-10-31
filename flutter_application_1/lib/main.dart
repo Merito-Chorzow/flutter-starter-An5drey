257c4c4205d3b74c9eaadf33ed19311a68680b82
Andrii Popchuk <andriy.popchuk@gmail.com>
2025-10-31 16:56:54 +0100

    Initial commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'select.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GeoVibe',
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('lib/images/Geovibe background2.png', fit: BoxFit.cover),
+
+          Center(
+            child: SizedBox(
+              width: 150,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(
+                    context,
+                  ).push(MaterialPageRoute(builder: (_) => const SelectPage()));
+                },
+
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(24),
+                    side: const BorderSide(color: Color(0xFF3A3A3A), width: 1),
+                  ),
+
+                  padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+                ),
+                child: const Text(
+                  'Zacznij🚀',
+                  style: TextStyle(
+                    color: Color(0xFF3A3A3A),
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter_application_1/lib/select.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelectPage extends StatelessWidget {
+  const SelectPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: const Center(child: Text('wmfwemwkwembjowb')));
+  }
+}
